e9cc2107813d1b2622c1c1fe49f40d2a4b77b091
Thomas Seliger <tom@neovatar.org>
2017-05-21 20:44:03 +0200

    refactored for new docker host

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+DEST_DIR=/var/docker/build/varnish
+SSH_DEST=""
+case $1 in
+prod)
+  SSH_DEST="tom@manji"
+  ;;
+*)
+  SSH_DEST="tom@10.33.66.11"
+  ;;
+esac
+
+echo "=> rsync"
+echo "   src: $(pwd)"
+echo "   dst: $SSH_DEST:$DEST_DIR"
+rsync -rlptgo \
+  --delete \
+  --delete-excluded \
+  --exclude='/.git' \
+  --filter="dir-merge,- .gitignore" \
+ . $SSH_DEST:$DEST_DIR
+
+ssh -A $SSH_DEST "cd $DEST_DIR && sudo ./install.sh"
